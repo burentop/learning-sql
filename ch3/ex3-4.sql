b301950e79306190259c1bfda32054b22f4a9b79
Brent Perry <burentoperii@gmail.com>
2017-04-10 17:13:37 -0500

    add exercises 3-2 through 3-4

---
+++ ch3/ex3-4.sql
@@ -0,0 +1,5 @@
+SELECT p.product_cd, a.cust_id, a.avail_balance
+FROM product p INNER JOIN account a
+     ON p.product_cd = a.product_cd
+WHERE p.product_type_cd = 'ACCOUNT'
+ORDER BY p.product_cd, a.cust_id;
